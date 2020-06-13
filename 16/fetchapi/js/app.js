47130081fd87d8a5d3c2c28d2f0acc433d9b7854
Diana Karen Parra Sosa <dianaparra092@gmail.com>
2020-06-12 23:40:28 -0500

    Cargando un JSON con Fetch API

--- 16/fetchapi/js/app.js
+++ 16/fetchapi/js/app.js
@@ -1,4 +1,5 @@
 document.getElementById('txtBtn').addEventListener('click', cargarTXT);
+document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
 
 
 
@@ -10,6 +11,25 @@ function cargarTXT() {
         .then(function(data) {
             console.log(data);
             document.getElementById('resultado').innerHTML = data;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+function cargarJSON() {
+    fetch('empleados.json')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data){
+            let html = '';
+            data.forEach(function(empleado) {
+                html += `
+                    <li>${empleado.nombre}  ${empleado.puesto}</li>
+                `;
+            })
+            document.getElementById('resultado').innerHTML = html;
         })
         .catch(function(error) {
             console.log(error);
